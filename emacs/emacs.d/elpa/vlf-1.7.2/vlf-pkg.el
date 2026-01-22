d0ee5c391dc9342752a4e24a9cc321d6138659d7
eegrok <304507+eegrok@users.noreply.github.com>
2026-01-22 16:50:16 -0500

    add vlf (very large files) mode for emacs

---
+++ emacs/emacs.d/elpa/vlf-1.7.2/vlf-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from vlf.el  -*- no-byte-compile: t -*-
+(define-package "vlf" "1.7.2" "View Large Files" 'nil :commit "efffeb5f54191d41a503d1d51343bb327fe2f871" :maintainer '("Andrey Kotlarski" . "m00naticus@gmail.com") :keywords '("large files" "utilities") :url "https://github.com/m00natic/vlfi")
